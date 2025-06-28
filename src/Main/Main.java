246468c3ae8492a46812d5f0f35ed2749eefd13e
Administrator <truongsonkmhd@gmail.com>
2025-06-28 22:43:54 +0700

    tuan 1

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,14 +1,27 @@
+package Main;
 
-package quanlycangtinuneti;
+import QuanLyPizza.DAO.MyConnect;
+import QuanLyPizza.GUI.DangNhapGUI;
 
+public class Main {
 
-public class QuanLyCangTinUneti {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new MyConnect();
+
+        changLNF("Nimbus");
+        DangNhapGUI login = new DangNhapGUI();
+        login.showWindow();
     }
-    
+
+    public static void changLNF(String nameLNF) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if (nameLNF.equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+        }
+    }
 }
